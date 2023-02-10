6abf2896ba541952df7811a3135225571d966898
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-10 21:50:23 -0200

    Update ExercicioPython09.py

--- ExercicioPython09.py
+++ ExercicioPython09.py
@@ -13,7 +13,7 @@ f1 = 9 * c / 5 + 32
 print('A temperatura de {}°C corresponde a {}°F' .format(c, f))
 print('A temperatura de {}°C corresponde a {}°F' .format(c, f1))
 
-# 15- Escreva um programa que pergunte a quantidade Km percorridos por um carro alugado e a quantidade de dias pelo quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custo R$ 60,00 por dia e R$ 0,15 Km rodado
+# 15- Escreva um programa que pergunte a quantidade Km percorridos por um carro alugado e a quantidade de dias pelo quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60,00 por dia com 0,15 Km rodado
 
 dias = int(input('Quantos dias alugados ? '))
 km = float(input('Quantos Km rodados ? '))
