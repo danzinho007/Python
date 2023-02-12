a0d73d478a8dcd0061cd54755da0f93bf9f83cab
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-12 10:05:46 -0200

    Create ExercicioPython101.py

---
+++ ExercicioPython101.py
@@ -0,0 +1,19 @@
+# Tipos Embutidos :
+# https://docs.python.org/pt-br/3/library/stdtypes.html 
+
+# Métodos de string :
+
+# isalnum : Já vi
+# isalpha : Já vi
+# isascii : Já vi
+# isdecimal : Já vi
+# isdigit : Já vi
+# isidentifier : Já vi
+
+# islower : Já vi
+# isnumeric : Já vi
+# isprintable : Já vi
+# isspace : Já vi
+# istitle : Já vi
+# isupper : Já vi
+
