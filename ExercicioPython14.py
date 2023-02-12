8e92c2d644d5e8b65408131c238f991c43ba9634
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-12 10:05:56 -0200

    Update ExercicioPython14.py

--- ExercicioPython14.py
+++ ExercicioPython14.py
@@ -1,20 +1,2 @@
-#19- Um professor quer sortear um dos seus 4 alunos para apagar o quadro. Faça um programa que ajude ele lendo o nome deles e escrevendo o nome do escolhido
-
-import math
-n1 = input('Digite aqui o nome do 1° aluno : ')
-n2 = input('Digite aqui o nome do 2°       : ')
-n3 = input('Digite aqui o nome do 3°       : ')
-n4 = input('Digite aqui o nome do 4°       : ')
-n5 = input('Digite aqui o nome do 5°       : ')
-
-import random
-sorteio = random.randint(1, 5)
-n1 = 1
-n2 = 2
-n3 = 3
-n4 = 4
-n5 = 5
-print('A pessoa sorteada é : {}' .format(sorteio))
-
-# ou
+# Executar com F5
 
