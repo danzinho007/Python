2154c133f1a8f8c7b32a884bacf4a83fd6894b6b
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-15 00:18:23 -0200

    alteracoes

--- ExercicioPython17.py
+++ ExercicioPython17.py
@@ -1,1 +1,30 @@
 # Executar com F5
+
+#Desafios
+
+#17- Faça um programa que leia o comprimento do Cateto Oposto e do Cateto Adjacente de um Triângulo Retângulo, calcule e mostre o comprimento da hipotenusa
+
+# Hipotenusa é a Raiz Quadrada da Soma dos Catetos ( Oposto e Adjacente ) ao Quadrado 
+# ou >>> Quadrado da Hipotenusa é igual a Soma do Quadrado dos Catetos
+#  Ou seja >>> Hip = Raiz Quadrada ( Co² + Ca² )
+
+co = float(input('Diga o Cateto Oposto : '))
+ca = float(input('Diga o Cateto Adjacente : '))
+hip = (co ** 2 + ca ** 2) ** ( 1/2 )
+print('A hipotenusa vale {:.2f}' .format(hip))
+
+# Ou
+
+from math import hypot
+co2 = float(input('Diga o Cateto Oposto : '))
+ca2 = float(input('Diga o Cateto Adjacente : '))
+hip2 = math.hypot(co2, ca2)
+print('A hipotenusa vale {:.2f}' .format(hip2))
+
+# Ou
+
+import math
+co1 = float(input('Diga o Cateto Oposto : '))
+ca1 = float(input('Diga o Cateto Adjacente : '))
+hip1 = math.hypot(co1, ca1)
+print('A hipotenusa vale {:.2f}' .format(hip1))
--- ExercicioPython18.py
+++ ExercicioPython18.py
@@ -1,1 +1,70 @@
 # Executar com F5
+
+# Comentário
+
+#18- Faça um programa que leia um ângulo e mostre na tela o valor do :
+#Seno, Cosseno, Tangente, Secante, Cossecante e Cotangente
+
+# Importando só o que vai usar :
+
+from math import radians, sin, cos, tan
+
+angulo = float(input('Informe o ângulo : '))
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
+secante = 1 / cosseno
+cossecante = 1 / seno
+cotangente = 1 / tangente
+cotangente1 = 1 / tan(radians(angulo))
+print('O valor do Seno é {:.2f}, \n Cosseno é {:.2f}, \n Tangente é {:.2f}, \n Secante é {:.2F}, \n Cossecante é {:.2F} e \n Cotangente é {:.2F}' .format(seno, cosseno, tangente, secante, cossecante, cotangente))
+print('A tangente vale : {:.2f}' .format(cotangente1))
+print(' ')
+
+# Jeito certo :
+
+import math
+
+ang = float(input('Informe o ângulo : '))
+n11 = math.sin(math.radians(ang))
+n22 = math.cos(math.radians(ang))
+n33 = math.tan(math.radians(ang))
+n44 = 1 / n22
+n55 = 1 / n11
+n66 = 1 / n33
+n77 = 1 / math.tan(math.radians(ang))
+print('O valor do Seno é {:.2f}, \n Cosseno é {:.2f}, \n Tangente é {:.2f}, \n Secante é {:.2F}, \n Cossecante é {:.2F} e \n Cotangente é {:.2F}' .format(n11, n22, n33, n44, n55, n66))
+print('A tangente vale : {:.2f}' .format(n77))
+print(' ')
+
+#Convertendo de Grau pra Radiano
+
+graus = 45
+radianos = graus * (math.pi / 180)
+seno = math.sin(radianos)
+print(seno)
+print(' ')
+
+# Fazendo direto :
+
+n1 = float(input('Informe o ângulo : '))
+n1 = math.sin(n1)
+# n1 ta armazenando o ângulo informado
+print('O valor do Seno é {:.2f}' .format(n1))
+ 
+n2 = math.cos(n1)
+#Retorna o cosseno de x radianos.
+print('O valor do Cosseno em radianos é {}' .format(n2))
+
+n3 = math.tan(n1)
+#Retorna o tangente de x radianos.
+print('O valor da tangente em radianos é {:.2f}' .format(n3))
+
+n4 = 1 / n2
+print('O valor da Secante é de {:.2f}' .format(n4))
+
+n5 = 1 / n1
+print('O valor da Cossecante é {:.2}' .format(n5))
+
+n6 = 1 / n3
+print('O valor da Cotangente é de {:.2f}' .format(n6))
