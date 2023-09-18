27c93a28d534b1bf31d3c850842d385b230a194a
danzinho007 <sedutordanzinho@hotmail.com>
2023-09-18 13:27:54 -0300

    add

---
+++ Python46.py
@@ -0,0 +1,17 @@
+# Quiz Python 
+# Qual é o valor de a após a execução do código ?
+
+a = 10
+b = a
+a = a * 2
+
+print(a)
+# Resultado 20
+# Após a execução do código fornecido, o valor de a será igual a 20. Aqui está o passo a passo da execução:
+# a é inicializado com o valor 10.
+# b é definido como igual a a, o que significa que b agora também tem o valor 10.
+# a é multiplicado por 2, resultando em a tendo o valor 20.
+
+print(b)
+# Resultado 10
+# O valor de b será igual a 10, pois foi inicialmente definido como igual a a quando a tinha o valor de 10, e a subsequente alteração de a para 20 não afeta o valor de b.
