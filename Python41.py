fc3b47495074232ad92491fb20b28d16db4dcc84
danzinho007 <sedutordanzinho@hotmail.com>
2023-09-20 01:33:47 -0300

    add

---
+++ Python41.py
@@ -0,0 +1,19 @@
+# Quiz Python
+# Qual é o valor de fruta após a execução do código ?
+
+fruta = "maçã"
+fruta = fruta + "s"
+fruta = fruta * 3
+
+print(fruta)
+# Resultado "maçãsmaçãsmaçãs"
+# Letra B
+
+# O valor de fruta após a execução do código será "maçãsmaçãsmaçãs".
+
+# Aqui está a explicação passo a passo:
+
+# Inicialmente, fruta é definido como "maçã".
+# Em seguida, a linha fruta = fruta + "s" concatena a string "s" à variável fruta, tornando-a "maçãs".
+# Por fim, a linha fruta = fruta * 3 multiplica a string "maçãs" por 3, resultando em "maçãsmaçãsmaçãs".
+# Portanto, quando você imprime fruta, verá a saída "maçãsmaçãsmaçãs".
---
+++ Python42.py
@@ -0,0 +1,17 @@
+# Quiz Python
+# Qual é o valor de cap2 ?
+
+cap1 = "Bangalore"
+cap2 = "Londres"
+cap3 = "Luanda"
+
+cap1 = cap2
+# cap1 se torna Londres
+cap2 = cap3
+# cap2 se torna Luanda
+cap3 = cap2 
+# cap 3 se torna Luanda
+cap2 = cap1
+# cap se torna Londres
+
+print(cap2)
