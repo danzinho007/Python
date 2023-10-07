b98945b0c7a4d00bb9c12bdb5914fb68863a04e3
danzinho007 <sedutordanzinho@hotmail.com>
2023-10-06 21:35:40 -0300

    add

--- Python01.py
+++ Python01.py
@@ -1,6 +1,81 @@
 # 01:
-# Guarda na variável uma mensagem
-# Exibe ela na tela
+# 1 Guarda na variável uma mensagem
+# 2 Exibe ela na tela
 
 msg = 'Olá Mundo'
 print(msg)
+
+msg0 = "Olá Mundo"
+print("O valor de msg0 é : " + msg0)
+
+#msg1 = Testando
+#print(msg1)
+
+# A variável não reconhece a string
+
+#msg2 = Oi voce
+#print(msg2)
+
+# A variável não reconhece a string
+
+#msg3 = 2
+#print("O valor de msg3 é : " + msg3)
+
+#A mensagem de erro "can only concatenate str (not 'int') to str" ocorre quando você está tentando concatenar (juntar) uma string com um valor inteiro em Python usando o operador de adição (+) direto. Em Python, você não pode concatenar diretamente uma string com um número inteiro, a menos que você converta o número inteiro em uma string antes de realizar a concatenação.
+
+#msg4 = +
+#print(msg4)
+# Essa mensagem de erro indica que o compilador ou interpretador estava esperando encontrar uma expressão válida em um determinado ponto do código, mas não encontrou o que esperava.
+
+msg5 = 1 + 1 # Adição
+print("O valor de msg5 é : " + str((msg5)))
+# A mensagem de erro "can only concatenate str (not 'int') to str" em Python ocorre quando você está tentando concatenar (juntar) uma string com um valor inteiro usando o operador de adição (+) direto, mas a operação não é permitida. Isso acontece porque, em Python, você não pode concatenar diretamente uma string com um número inteiro sem convertê-lo em uma string primeiro.
+print(msg5)
+
+# Operações :
+#soma = n2 + n1
+#sub = n2 - n1
+#mult = n2 * n1
+#div = n2 / n1
+#divInt = n2 // n1
+#resto = n2 % n1
+#pot = n2 ** n1
+
+msg6 = 1 - 1 # Subtração
+print(msg6)
+
+msg7 = 1 * 1 # Multiplicação
+print(msg7)
+
+msg8 = 1 / 1 # Divisão
+print(msg8)
+
+msg9 = 4 // 2 # Divisão Inteira ou Divisor
+print(msg9)
+# Dividendo | Diviso
+# Resto     | Quociente
+
+msg10 = 1 % 1 # Resto da Divisão
+print(msg10)
+
+msg11 = 1 ** 1 # Potenciação
+print(msg11)
+
+msg12 = 1 ** ( 1/2 ) # Raiz Quadrada
+print(msg12)
+
+msg13 = 2 ** ( 3/3 )
+print(msg13)
+
+# 1/2 : Numerador 1 
+#       Denominador 2
+
+# 1 elevado a 1/2  = Raiz Quadrada de 1 > 1
+# 4 elevado a 1/2  = ... ... de 4 > 2
+# 27 elevado a 1/3 = ... Cúbica de 27 > 3
+#  2 elevado a 2/3 = ... ... de 2 elevado a 2
+# Resposta : raiz cúbica de 4
+#  2 elevado a 3/3 = ... ... de 2 elevado a 3
+# Resposta : 2³ = 2 x 2 ( 4 ) x 2 ( 8 ) > 2
+#  2 elevado a 4/3 = ... ... de 2 elevado a 4
+# Resposta : 2 x 2 ( 4 ) x 2 ( 8 ) x 2 ( 16 ) 
